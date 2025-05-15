69ee8e7077cd1a10555bb7e5515a96687900315f
Steven Simpson <568737+simpsonst@users.noreply.github.com>
2025-05-15 15:00:01 +0100

    Common utilities for main programs.

---
+++ src/python/apps/lancs_gridmon/apps.py
@@ -0,0 +1,78 @@
+## Copyright (c) 2022, Lancaster University
+## All rights reserved.
+##
+## Redistribution and use in source and binary forms, with or without
+## modification, are permitted provided that the following conditions
+## are met:
+##
+## 1. Redistributions of source code must retain the above copyright
+##    notice, this list of conditions and the following disclaimer.
+##
+## 2. Redistributions in binary form must reproduce the above
+##    copyright notice, this list of conditions and the following
+##    disclaimer in the documentation and/or other materials provided
+##    with the distribution.
+##
+## 3. Neither the name of the copyright holder nor the names of its
+##    contributors may be used to endorse or promote products derived
+##    from this software without specific prior written permission.
+##
+## THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+## "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+## LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+## FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+## COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+## INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+## (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+## SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+## HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+## STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+## ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+## OF THE POSSIBILITY OF SUCH DAMAGE.
+
+import os
+import sys
+import logging
+import signal
+
+def silence_output():
+    ## Mainly for use with cron, send stdout and stderr to /dev/null.
+    with open('/dev/null', 'w') as devnull:
+        fd = devnull.fileno()
+        os.dup2(fd, sys.stdout.fileno())
+        os.dup2(fd, sys.stderr.fileno())
+        pass
+    pass
+
+def prepare_log_rotation(config):
+    logging.basicConfig(**config)
+    if 'filename' in config:
+        def handler(signum, frame):
+            logging.root.handlers = []
+            logging.basicConfig(**config)
+            logging.info('rotation')
+            pass
+        signal.signal(signal.SIGHUP, handler)
+        pass
+    pass
+
+class ProcessIDFile:
+    def __init__(self, fn):
+        self.fn = fn
+        pass
+
+    def __enter__(self):
+        if self.fn is None:
+            return
+        with open(self.fn, "w") as f:
+            f.write('%d\n' % os.getpid())
+            pass
+        pass
+
+    def __exit__(self, typ, val, tb):
+        if self.fn is None:
+            os.remove(self.fn)
+            return
+        pass
+
+    pass
