5fdb3ab611c1b8cc8fe119bad493c25723623b7c
Steven Simpson <568737+simpsonst@users.noreply.github.com>
2025-05-20 15:47:10 +0100

    Extracted peer-management functionality from old collector.

---
+++ src/python/apps/lancs_gridmon/xrootd/detail/management.py
@@ -0,0 +1,118 @@
+## Copyright (c) 2022, Lancaster University
+## All rights reserved.
+##
+## Redistribution and use in source and binary forms, with or without
+## modification, are permitted provided that the following conditions
+## are met:
+##
+## 1. Redistributions of source code must retain the above copyright
+##    notice, this list of conditions and the following disclaimer.
+##
+## 2. Redistributions in binary form must reproduce the above
+##    copyright notice, this list of conditions and the following
+##    disclaimer in the documentation and/or other materials provided
+##    with the distribution.
+##
+## 3. Neither the name of the copyright holder nor the names of its
+##    contributors may be used to endorse or promote products derived
+##    from this software without specific prior written permission.
+##
+## THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+## "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+## LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+## FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+## COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+## INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+## (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+## SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+## HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+## STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+## ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+## OF THE POSSIBILITY OF SUCH DAMAGE.
+
+from lancs_gridmon.xrootd.detail.recordings import Recorder as XRootDRecorder
+
+class PeerManager:
+    def __init__(self, now, evrec, adv, domains=None, id_to_min=120,
+                 seq_to=2):
+        self._evrec = evrec
+        self._adv = adv
+        self._domains = domains
+
+        ## We map from client host/port to Peer.
+        self._peers = { }
+
+        ## When we get an identity, we map it to the client host/port
+        ## here.  If the old value is different, we purge the old
+        ## value from self._peers.
+        self._names = { }
+
+        ## Set the timeout for ids.  Remember when we last purged
+        ## them.
+        self._id_to = id_to_min * 60
+        self._id_ts = now
+
+        ## Set the timeout for missing sequence numbers.
+        self._seq_to = seq_to
+
+        pass
+
+    def _identify(self, pgm, host, inst, peer):
+        ## Check to see if anything has changed.
+        key = (host, inst, pgm)
+        old_addr = self._names.get(key)
+        if old_addr is not None:
+            old = self._peers.get(old_addr)
+            if old is not None and old == peer:
+                return
+
+        ## Replace the old entry.
+        self._names[key] = peer
+        peer.set_identity(host, inst, pgm)
+        old = self._peers.pop(old_addr, None)
+        if old is not None:
+            old.discard()
+            pass
+        pass
+
+    def process(self, now, addr, dgram):
+        try:
+            ## TODO: Check if addr[0] is in permitted set.
+
+            msg = dgram['message']
+            stod = msg['stod']
+            addr = (msg['peer']['host'], msg['peer']['port'])
+            pseq = msg['pseq']
+
+            ## Locate the peer record.  Replace with a new one if the
+            ## start time has increased.
+            peer = self._peers.get(addr)
+            if peer is None or stod > peer.stod:
+                self._peers[addr] = peer = \
+                    Peer(stod, addr, self._identify, self._evrec,
+                         id_timeout=self._id_to,
+                         seq_timeout=self._seq_to,
+                         domains=self._domains)
+                self.check_identity()
+            elif stod < peer.stod:
+                ## Ignore messages from old instances.
+                return
+
+            ## Submit the message to be incorporated into the peer
+            ## record.
+            peer.process(now, pseq, msg)
+        except Exception as e:
+            logging.error('error processing %s' % dgram)
+            raise e
+        finally:
+            if now - self._id_ts > self._id_to:
+                for addr, peer in self._peers.items():
+                    peer.id_clear(now)
+                    continue
+                self._id_ts = now
+                pass
+            self._adv(now)
+            pass
+        return
+
+    pass
