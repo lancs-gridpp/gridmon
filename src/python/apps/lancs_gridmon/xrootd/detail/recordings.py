61b27c796d570e2453eabb5c41b9b9fc0ad45f94
Steven Simpson <568737+simpsonst@users.noreply.github.com>
2025-05-20 14:52:51 +0100

    Fixed imports.

--- src/python/apps/lancs_gridmon/xrootd/detail/recordings.py
+++ src/python/apps/lancs_gridmon/xrootd/detail/recordings.py
@@ -30,6 +30,8 @@
 ## ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
 ## OF THE POSSIBILITY OF SUCH DAMAGE.
 
+import os
+from datetime import datetime
 import lancs_gridmon.logfmt as logfmt
 
 class Recorder:
